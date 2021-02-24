44e24e9596280b6b8c65da13c51cc50e73539208
Florijan Hamzic <fh@infinicode.de>
2021-02-24 15:46:14 +0100

    add new taskwarrior service & subprocess helper

---
+++ taskwhisperer-extension@infinicode.de/helpers/subprocess.js
@@ -0,0 +1,64 @@
+const { Gio, GLib } = imports.gi
+
+const ExtensionUtils = imports.misc.extensionUtils
+const Me = ExtensionUtils.getCurrentExtension()
+
+const { clearTimeout, setTimeout } = Me.imports.helpers.components
+const { tryJsonParse } = Me.imports.helpers.data
+
+// partially copied from https://wiki.gnome.org/AndyHolmes/Sandbox/SpawningProcesses
+var run = async ({ command, asJson = true, input = null, timeout = 10 }) => {
+  try {
+    const [ok, argv] = GLib.shell_parse_argv(command)
+
+    // We'll assume we want output, or that returning none is not a problem
+    let flags = Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+
+    // If we aren't given any input, we don't need to open stdin
+    if (input !== null) {
+      flags |= Gio.SubprocessFlags.STDIN_PIPE
+    }
+
+    const proc = new Gio.Subprocess({
+      argv,
+      flags
+    })
+
+    const cancellable = new Gio.Cancellable()
+
+    // Classes that implement GInitable must be initialized before use, but
+    // an alternative in GJS is to just use Gio.Subprocess.new(argv, flags)
+    proc.init(cancellable)
+
+    const cancelTimeOutId = setTimeout(() => cancellable.cancel(), timeout * 1000)
+
+    const result = await new Promise((resolve, reject) => {
+      proc.communicate_utf8_async(input, cancellable, (proc, res) => {
+        try {
+          let [ok, stdout, stderr] = proc.communicate_utf8_finish(res)
+
+          if (asJson) {
+            stdout = tryJsonParse(stdout)
+          }
+
+          const returnCode = proc.get_exit_status()
+
+          if (returnCode !== 0) {
+            resolve({ error: stderr || stdout })
+          }
+
+          clearTimeout(cancelTimeOutId)
+          resolve({ output: stdout })
+        } catch (e) {
+          logError(e)
+          reject(e)
+        }
+      })
+    })
+
+    return result
+  } catch (e) {
+    logError(e)
+    return { error: e.toString() }
+  }
+}
---
+++ taskwhisperer-extension@infinicode.de/services/taskService.js
@@ -0,0 +1,279 @@
+/* jshint esnext:true */
+/*
+ *
+ *  GNOME Shell Extension for the great Taskwarrior application
+ *  - Displays pending Tasks.
+ *  - adding / modifieing tasks.
+ *
+ * Copyright (C) 2019
+ *     Florijan Hamzic <fh@infinicode.de>,
+ *
+ * This file is part of gnome-shell-extension-taskwhisperer.
+ *
+ * gnome-shell-extension-taskwhisperer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gnome-shell-extension-taskwhisperer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gnome-shell-extension-taskwhisperer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+const ExtensionUtils = imports.misc.extensionUtils
+const Me = ExtensionUtils.getCurrentExtension()
+
+const { showNotification } = Me.imports.helpers.components
+const { run } = Me.imports.helpers.subprocess
+
+const { Task } = Me.imports.services.dto.task
+const { TaskStatus, TaskOrder } = Me.imports.services.meta.taskWarrior
+const { Settings } = Me.imports.helpers.settings
+
+var loadTaskData = async ({ taskStatus, project, taskOrder }) => {
+  let statusFilter = 'status:Pending'
+  let projectFilter = ''
+
+  switch (taskStatus) {
+    case TaskStatus.PENDING:
+      statusFilter = 'status:Pending'
+      break
+    case TaskStatus.COMPLETED:
+      statusFilter = 'status:Completed'
+      break
+  }
+
+  if (project) {
+    switch (project) {
+      case 'all':
+        break
+      case 'unassigned':
+        projectFilter = `project:`
+        break
+      default:
+        projectFilter = `project:'${project}'`
+        break
+    }
+  }
+
+  const command = ['task', 'rc.json.array=on', statusFilter, projectFilter, 'export'].join(' ')
+
+  let { output, error } = await run({ command })
+
+  let tasks
+
+  if (output) {
+    try {
+      tasks = output.map(taskData => new Task(taskData))
+
+      switch (taskOrder) {
+        case TaskOrder.DUE:
+          if (taskStatus === TaskStatus.COMPLETED) {
+            tasks.sort(_sortByModification)
+          } else {
+            tasks.sort(_sortByDue)
+          }
+          break
+
+        case TaskOrder.URGENCY:
+          tasks.sort(_sortByUrgency)
+          break
+      }
+    } catch (e) {
+      logError(e)
+      error = e
+    }
+  }
+
+  return { tasks, error }
+}
+
+var loadProjectsData = async taskStatus => {
+  let statusFilter = 'status:Pending'
+
+  switch (taskStatus) {
+    case TaskStatus.PENDING:
+      statusFilter = 'status:Pending'
+      break
+
+    case TaskStatus.COMPLETED:
+      statusFilter = 'status:Completed'
+      break
+  }
+
+  const command = ['task', 'rc.json.array=on', statusFilter, 'export'].join(' ')
+  const { output: allTheTasks } = await run({ command })
+
+  let sortedUniqueProjects = []
+
+  try {
+    sortedUniqueProjects = [...new Set(allTheTasks.filter(item => item.project).map(item => item.project).sort())]
+  } catch (e) {
+    logError(e)
+  }
+
+  return sortedUniqueProjects
+}
+
+var setTaskDone = async taskID => {
+  if (!taskID) {
+    return
+  }
+
+  const command = ['task', taskID.toString(), 'done'].join(' ')
+  const result = await run({ command, asJson: false })
+
+  _showProcessErrorNotificationIfError(result)
+
+  return result
+}
+
+var setTaskUndone = async taskUUID => {
+  if (!taskUUID) {
+    return
+  }
+
+  const command = ['task', `uuid:${taskUUID}`, 'modify', 'status:pending'].join(' ')
+  const result = await run({ command, asJson: false })
+
+  _showProcessErrorNotificationIfError(result)
+
+  return result
+}
+
+var startTask = async taskID => {
+  if (!taskID) {
+    return
+  }
+
+  const command = ['task', taskID.toString(), 'start'].join(' ')
+  const result = await run({ command, asJson: false })
+
+  _showProcessErrorNotificationIfError(result)
+
+  return result
+}
+
+var stopTask = async taskID => {
+  if (!taskID) {
+    return
+  }
+
+  const command = ['task', taskID.toString(), 'stop'].join(' ')
+  const result = await run({ command, asJson: false })
+
+  _showProcessErrorNotificationIfError(result)
+
+  return result
+}
+
+var createTask = async task => {
+  const params = _convertTaskToParams(task)
+
+  const command = ['task', 'add', ...params].join(' ')
+  const result = await run({ command, asJson: false })
+
+  return result
+}
+
+var modifyTask = async (taskUUID, task) => {
+  if (!taskUUID) {
+    return
+  }
+
+  const params = _convertTaskToParams(task)
+
+  const command = ['task', `uuid:${taskUUID}`, 'modify', ...params].join(' ')
+  const result = await run({ command, asJson: false })
+
+  return result
+}
+
+var syncTasks = async () => {
+  const command = ['task', 'sync'].join(' ')
+  const result = await run({ command, asJson: false })
+
+  _showProcessErrorNotificationIfError(result)
+
+  return result
+}
+
+const _showProcessErrorNotificationIfError = ({ error }) => {
+  if (error) {
+    showNotification({ title: 'TaskWarrior Error', message: error })
+  }
+}
+
+const _convertTaskToParams = task => {
+  const params = []
+
+  if (!task) {
+    return params
+  }
+
+  if (task.project) {
+    params.push(`project:${task.project}`)
+  }
+
+  if (task.description) {
+    params.push(task.description)
+  }
+
+  if (task.tags) {
+    params.push(`tags:${task.tags}`)
+  }
+
+  if (task.due) {
+    params.push(`due:${task.due}`)
+  }
+
+  if (task.priority) {
+    params.push(`priority:${task.priority}`)
+  }
+
+  if (task.additional) {
+    params.push(task.additional)
+  }
+
+  return params
+}
+
+const _sortByDue = (a, b) => {
+  let dueA
+  let dueB
+
+  if (Settings.show_no_dates_at_end) {
+    dueA = a.Due || '999999999999999'
+    dueB = b.Due || '999999999999999'
+  } else {
+    dueA = a.Due || ''
+    dueB = b.Due || ''
+  }
+
+  dueA = dueA.replace('T', '').replace('Z', '')
+  dueB = dueB.replace('T', '').replace('Z', '')
+
+  return dueA - dueB
+}
+
+const _sortByModification = (a, b) => {
+  let valueA = a.Modified || ''
+  let valueB = b.Modified || ''
+
+  valueA = valueA.replace('T', '').replace('Z', '')
+  valueB = valueB.replace('T', '').replace('Z', '')
+
+  return valueB - valueA
+}
+
+const _sortByUrgency = (a, b) => {
+  let valueA = a.Urgency || ''
+  let valueB = b.Urgency || ''
+
+  return valueB - valueA
+}
